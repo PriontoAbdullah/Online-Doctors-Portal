54ced89c72b4f22e1ebae2b4f052170722ea3e4e
PriontoAbdullah <priontosoft@gmail.com>
2021-01-15 00:25:51 +0600

    Doctor's portal view  complete

--- src/Components/Doctor/Doctor.js
+++ src/Components/Doctor/Doctor.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import img from '../../images/doctor-sm.png'
+import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
-const Doctor = () => {
-    return (
-        <div className="col-md-4 text-center">
-            <img className="img-fluid mb-3" src={img} alt=""/>
-            <h4>Dr. Coudi</h4>
-            <p> <FontAwesomeIcon className="text-primary" icon={faPhoneAlt}/> +880-188-934789</p>
-        </div>
-    );
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../Doctors/Doctors.css';
+
+const Doctor = ({ doctorsData }) => {
+	const { img, category, name, education, designation, department, hospital } = doctorsData;
+	const [ descriptionCollapse, setDescriptionCollapse ] = useState(false);
+
+	const showMore = () => {
+		setDescriptionCollapse(true);
+	};
+
+	const showLess = () => {
+		setDescriptionCollapse(false);
+	};
+
+	return (
+		<div className="single-doctor">
+			<img className="img-fluid doctor-image" src={img} alt="doctor" />
+			<div className="doctor-description">
+				<p className="doctor-category">{category}</p>
+				<h4 className="doctor-name">{name}</h4>
+				<span className="doctor-education">{descriptionCollapse ? education : education.substr(0, 80)}</span>
+				{education.length > 80 ? descriptionCollapse ? (
+					<span onClick={showLess} className="collapse-btn">
+						See Less
+					</span>
+				) : (
+					<span onClick={showMore} className="collapse-btn">
+						See More
+					</span>
+				) : (
+					<span> </span>
+				)}
+				<h6 className="mt-4">{designation}</h6>
+				<h6 className="department">{department}</h6>
+				<h6 className="hospital">{hospital}</h6>
+				<div className="text-center">
+					<Link to="">
+						<button className="btn btn-primary button-style mt-3">
+							<FontAwesomeIcon icon={faCalendarCheck} className="mr-3" /> Make Appointment
+						</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Doctor;
--- src/Components/Doctors/Doctors.js
+++ src/Components/Doctors/Doctors.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SwiperCore, { A11y, Autoplay, Navigation, Pagination, Scrollbar, Virtual } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+import doctors from '../../Data/doctors';
 import Doctor from '../Doctor/Doctor';
+import './Doctors.css';
+
+SwiperCore.use([ Navigation, Pagination, Scrollbar, A11y, Autoplay, Virtual ]);
+
 const Doctors = () => {
+
+    const [ doctorsData, setDoctorsData ] = useState();
+
+	useEffect(() => {
+		setDoctorsData(doctors);
+    }, []);
+    
     return (
         <section className="doctors">
             <div className="container my-5">
                 <h1 className="text-center text-primary my-5">Our Doctors</h1>
-                <div className="row">
-                    <Doctor/>
-                    <Doctor/>
-                    <Doctor/>
+                <div className="doctors-view">
+                    <Swiper
+					spaceBetween={20}
+                    slidesPerView={3}
+                    centeredslide="true"
+					navigation
+					autoplay={true}
+					key={doctorsData && doctorsData.length}
+				>
+					{doctorsData &&
+						doctorsData.map((doctorsData, index) => (
+							<SwiperSlide key={index}>
+								<Doctor key={doctorsData.id} doctorsData={doctorsData} />
+							</SwiperSlide>
+						))}
+				</Swiper>
                 </div>
             </div>
         </section>
